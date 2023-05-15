617212f96b159d13b98c8ced81013817f0242935
David Andersson <david@jdkandersson.com>
2023-05-15 17:11:51 +1000

    fix not required signatures

--- tests/integration/test_source_branch_protection.py
+++ tests/integration/test_source_branch_protection.py
@@ -8,11 +8,13 @@ from uuid import uuid4
 
 import pytest
 from github import Github
+from github.Branch import Branch
 from github.Repository import Repository
 
 from repo_policy_compliance import Result, source_branch_protection
 
 from .. import assert_
+from . import branch_protection
 from .types_ import BranchWithProtection
 
 
@@ -78,22 +80,44 @@ def test_fail(
     assert report.result == Result.FAIL
 
 
-@pytest.mark.parametrize(
-    "github_branch",
-    [
-        BranchWithProtection(
-            name=f"protected/{uuid4()}",
-            require_code_owner_reviews=False,
-            dismiss_stale_reviews_enabled=False,
-            bypass_pull_request_allowance_disabled=False,
-        )
-    ],
-    indirect=True,
-)
-def test_pass(
+@pytest.fixture(name="source_branch_for_test_pass")
+def fixture_source_branch_for_test_pass(
     github_repository: Repository,
     ci_github_token: str | None,
-    github_branch: BranchWithProtection,
+    github_repository_name: str,
+):
+    """Create branch for the test_pass test."""
+    branch_name = f"source-branch/protected/{uuid4()}"
+    main_branch = github_repository.get_branch(github_repository.default_branch)
+    branch_ref = github_repository.create_git_ref(
+        ref=f"refs/heads/{branch_name}", sha=main_branch.commit.sha
+    )
+    branch = github_repository.get_branch(branch_name)
+
+    # Use the CI GitHub token to create a signed commit only in CI
+    if ci_github_token:
+        ci_github_client = Github(login_or_token=ci_github_token)
+        ci_repository = ci_github_client.get_repo(github_repository_name)
+        ci_repository.create_file("test.txt", "testing", "some content", branch=branch_name)
+
+    branch_with_protection = BranchWithProtection(
+        name=branch_name,
+        require_code_owner_reviews=False,
+        dismiss_stale_reviews_enabled=False,
+        bypass_pull_request_allowance_disabled=True,
+    )
+
+    branch_protection.edit(branch=branch, branch_with_protection=branch_with_protection)
+
+    yield branch
+
+    branch.remove_protection()
+    branch_ref.delete()
+
+
+def test_pass(
+    github_repository: Repository,
+    source_branch_for_test_pass: Branch,
     github_repository_name: str,
 ):
     """
@@ -101,16 +125,10 @@ def test_pass(
     act: when source_branch_protection is called with the name of the branch.
     assert: then a pass report is returned.
     """
-    # Use the CI GitHub token to create a signed commit only in CI
-    if ci_github_token:
-        ci_github_client = Github(login_or_token=ci_github_token)
-        ci_repository = ci_github_client.get_repo(github_repository_name)
-        ci_repository.create_file("test.txt", "testing", "some content", branch=github_branch.name)
-
     # The github_client is injected
     report = source_branch_protection(  # pylint: disable=no-value-for-parameter
         repository_name=github_repository_name,
-        branch_name=github_branch.name,
+        branch_name=source_branch_for_test_pass.name,
         target_branch_name=github_repository.default_branch,
     )
 
