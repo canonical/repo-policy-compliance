5bb5763d7672767b2314ef40dabaf1ee00c08bcb
David Andersson <david@jdkandersson.com>
2023-09-12 16:03:11 +1000

    fix linting issue

--- generate-src-docs.sh
+++ generate-src-docs.sh
@@ -7,5 +7,5 @@ rm -rf src-docs
 # There is a problem with lazydocs and using the flask Blueprint class which is causing an error,
 # skip that file
 PYTHON_FILES=$(find repo_policy_compliance -name "*.py" -not -path "**/blueprint.py")
-echo $PYTHON_FILES
-lazydocs --no-watermark --output-path src-docs $PYTHON_FILES
+# The word splitting is ententional here
+lazydocs --no-watermark --output-path src-docs $PYTHON_FILES  # shellcheck disable=SC2086
