e27987e80b41b5b2a79888b4c7e5a674f3ca8e05
David Andersson <david@jdkandersson.com>
2023-09-12 16:27:43 +1000

    fix spelling mistake

--- generate-src-docs.sh
+++ generate-src-docs.sh
@@ -7,6 +7,6 @@ rm -rf src-docs
 # There is a problem with lazydocs and using the flask Blueprint class which is causing an error,
 # skip that file
 PYTHON_FILES=$(find repo_policy_compliance -name "*.py" -not -path "**/blueprint.py")
-# The word splitting is ententional here
+# The word splitting is intentional here
 # shellcheck disable=SC2086
 lazydocs --no-watermark --output-path src-docs $PYTHON_FILES
