060d1e68b024fd350d925c1f8bab00a6d3b3c797
David Andersson <david@jdkandersson.com>
2023-09-12 15:41:42 +1000

    fix linting issus

--- generate-src-docs.sh
+++ generate-src-docs.sh
@@ -6,5 +6,6 @@
 rm -rf src-docs
 # There is a problem with lazydocs and using the flask Blueprint class which is causing an error,
 # skip that file
-PYTHON_FILES=$(find repo_policy_compliance -name '*.py' -not -path *blueprint.py)
+PYTHON_FILES=$(find repo_policy_compliance -name "*.py" -not -path "**/blueprint.py")
+echo $PYTHON_FILES
 lazydocs --no-watermark --output-path src-docs $PYTHON_FILES
