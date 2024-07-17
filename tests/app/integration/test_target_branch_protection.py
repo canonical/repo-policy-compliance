4b5f0f8af8168899409bc62576be44d5104ce8d5
Christopher Bartz <christopher.bartz@canonical.com>
2024-07-17 22:19:22 +0200

    do not use the fork repo

--- tests/app/integration/test_target_branch_protection.py
+++ tests/app/integration/test_target_branch_protection.py
@@ -146,9 +146,7 @@ def test_fail_default_branch(
     assert repr(report) in caplog.text
 
 
-def test_fail_branch_missing(
-    forked_github_repository: Repository, caplog: pytest.LogCaptureFixture
-):
+def test_fail_branch_missing(github_repository_name: str, caplog: pytest.LogCaptureFixture):
     """
     arrange: given a branch that is missing.
     act: when target_branch_protection is called with the name of the branch.
@@ -157,9 +155,9 @@ def test_fail_branch_missing(
     branch_name = f"missing-branch-{uuid4()}"
     # The github_client is injected
     report = target_branch_protection(  # pylint: disable=no-value-for-parameter
-        repository_name=forked_github_repository.full_name,
+        repository_name=github_repository_name,
         branch_name=branch_name,
-        source_repository_name=forked_github_repository.full_name,
+        source_repository_name="other repository",
     )
 
     assert report.result == Result.FAIL
@@ -181,7 +179,7 @@ def test_fail_branch_missing(
 )
 @pytest.mark.usefixtures("protected_github_branch", "ruleset_protected_github_branch")
 def test_fail_branch_protection_using_rulesets(
-    github_branch: Branch, forked_github_repository: Repository, caplog: pytest.LogCaptureFixture
+    github_branch: Branch, github_repository_name: str, caplog: pytest.LogCaptureFixture
 ):
     """
     arrange: given a branch that is protected via ruleset and not the branch protection API.
@@ -190,9 +188,9 @@ def test_fail_branch_protection_using_rulesets(
     """
     # The github_client is injected
     report = target_branch_protection(  # pylint: disable=no-value-for-parameter
-        repository_name=forked_github_repository.full_name,
+        repository_name=github_repository_name,
         branch_name=github_branch.name,
-        source_repository_name=forked_github_repository.full_name,
+        source_repository_name="other repository",
     )
 
     assert report.result == Result.FAIL
